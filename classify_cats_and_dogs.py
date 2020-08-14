947b6abbc40228a7289d9034b5b3844f3a49edee
siddharth <siddharthsoni22@gmail.com>
2020-08-13 21:55:30 -0500

    Model trained with 12000 training images, l2 regularization, 15 epochs and batch size of 30

--- classify_cats_and_dogs.py
+++ classify_cats_and_dogs.py
@@ -6,23 +6,25 @@ from tensorflow.keras.optimizers import RMSprop
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from shutil import copyfile
+from keras.regularizers import l2
 os.environ["CUDA_VISIBLE_DEVICES"]="6"
 
+W_reg = 1e-4
 model = tf.keras.models.Sequential([
-    tf.keras.layers.Conv2D(64,(3,3), activation='relu', input_shape=(150,150,3)),
+    tf.keras.layers.Conv2D(64,(3,3), activation='relu', input_shape=(150,150,3),kernel_regularizer=l2(W_reg)),
     tf.keras.layers.MaxPooling2D(2,2),
     ## Second layer
-    tf.keras.layers.Conv2D(64,(3,3), activation='relu'),
+    tf.keras.layers.Conv2D(64,(3,3), activation='relu',kernel_regularizer=l2(W_reg)),
     tf.keras.layers.MaxPooling2D(2,2),
     ## Third layer
-    tf.keras.layers.Conv2D(128,(3,3), activation='relu'),
+    tf.keras.layers.Conv2D(128,(3,3), activation='relu',kernel_regularizer=l2(W_reg)),
     tf.keras.layers.MaxPooling2D(2,2),
     ## Fourth layer
-    tf.keras.layers.Conv2D(256,(3,3), activation='relu'),
+    tf.keras.layers.Conv2D(256,(3,3), activation='relu',kernel_regularizer=l2(W_reg)),
     tf.keras.layers.MaxPooling2D(2,2),
     ###Flatten
     tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128,activation='relu'),
+    tf.keras.layers.Dense(128,activation='relu',kernel_regularizer=l2(W_reg)),
     tf.keras.layers.Dropout(0.2),
     tf.keras.layers.Dense(64,activation='tanh'),
     tf.keras.layers.Dense(1,activation='sigmoid'),
@@ -32,18 +34,18 @@ model = tf.keras.models.Sequential([
 model.compile(optimizer=RMSprop(lr=0.001), loss='binary_crossentropy', metrics=['acc'])
 print(model.summary())
 TRAINING_DIR = '/home/siddharth.soni/cats_and_dogs/Training/'
-train_datagen = ImageDataGenerator(rescale=1.0/255.0,
-		width_shift_range=0.1, height_shift_range=0.1, 
+train_datagen = ImageDataGenerator(rescale=1.0/255.0)
+	#	width_shift_range=0.1, height_shift_range=0.1, 
 		#rotation_range=40,
         	#shear_range=0.2,
         	#zoom_range=0.2,
-        	horizontal_flip=True,
-       		fill_mode='nearest')
+        #	horizontal_flip=True,
+       	#	fill_mode='nearest')
 
 # TRAIN GENERATOR.
 train_generator = train_datagen.flow_from_directory(
                     TRAINING_DIR,
-                    batch_size=40,
+                    batch_size=30,
                     target_size=(150,150),
                     class_mode='binary'
                     )
@@ -53,14 +55,14 @@ validation_datagen = ImageDataGenerator(rescale=1.0/255.0)
 
 validation_generator = validation_datagen.flow_from_directory(
                         VALIDATION_DIR,
-                        batch_size=40,
+                        batch_size=30,
                         target_size=(150,150),
                         class_mode='binary')
 # number of images is 5400
-history = model.fit(train_generator,steps_per_epoch=5400//40, 
-                              epochs=30,
+history = model.fit(train_generator,steps_per_epoch=12000//30, 
+                              epochs=15,
                               verbose=1,
-                              validation_data=validation_generator,validation_steps=5400//40)
+                              validation_data=validation_generator,validation_steps=1200//30)
 acc = history.history['acc']
 val_acc = history.history['val_acc']
 loss = history.history['loss']
